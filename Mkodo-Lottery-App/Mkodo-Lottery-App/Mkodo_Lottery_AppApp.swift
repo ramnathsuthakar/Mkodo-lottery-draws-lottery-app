31a10019b8ad6f3e18e870166e4babb259d4e367
Ramanath Suthakar <ramnath.suthakar@googlemail.com>
2024-08-12 12:37:39 +0100

    Project initial commit

---
+++ Mkodo-Lottery-App/Mkodo-Lottery-App/Mkodo_Lottery_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mkodo_Lottery_AppApp.swift
+//  Mkodo-Lottery-App
+//
+//  Created by Ramanath Suthakar on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mkodo_Lottery_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
